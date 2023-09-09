781ec0946c5d07fa6ed15f48a0f3bd6c9d8f0a98
aditigaur824 <aditigaur824@gmail.com>
2023-09-09 15:14:47 -0700

    Added little explanation blurb

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import SymptomTracker from './components/SymptomTracker'
-import { ChakraProvider, CSSReset, extendTheme, Heading } from '@chakra-ui/react';
+import { ChakraProvider, CSSReset, extendTheme, Heading, Text } from '@chakra-ui/react';
 
 const theme = extendTheme({
   // Add custom Chakra UI theme settings if needed
@@ -21,6 +21,14 @@ export default function Home() {
         <Heading as="h1" size="xl" mb={8}>
           {`Your Intelligent Symptom Tracker`}
         </Heading>
+        <Text mb={4} maxWidth={"2xl"} fontSize={"sm"}>
+          {`We know how frustrating it can be to try to explain everything you've been feeling to your doctor. Sometimes you may not even know where to start. 
+            If you've been experiencing chronic symptoms, this tool can help you keep track of them, and when the time is right, analyze your symptoms, ask you follow ups
+            to get more information, and provide you with a summary that you can share with your doctor.
+
+            You can clear your symptoms at any time by deleting them. Once you're ready to analyze your symptoms, click "Analyze my Symptoms" to get started!
+          `}
+        </Text>
         <SymptomTracker />
       </ChakraProvider>
     </main>
