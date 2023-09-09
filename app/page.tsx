a17440314e46a034fcd16087db59f5a328a84861
aditigaur824 <aditigaur824@gmail.com>
2023-09-09 15:06:22 -0700

    Updating name and favicon.

--- app/page.tsx
+++ app/page.tsx
@@ -10,10 +10,16 @@ const theme = extendTheme({
 export default function Home() {
   return (
     <main className="flex min-h-screen w-screen flex-col items-center p-16 bg-gray-50">
+      <title>Your Symptom Tracker</title>
+      <meta
+        name="description"
+        content="An intelligent symptom tracker."
+      />
+
       <ChakraProvider theme={theme}>
         <CSSReset />
-        <Heading as="h1" size="2xl" mb={8}>
-          {`Ted Lasso's Symptom Tracker`}
+        <Heading as="h1" size="xl" mb={8}>
+          {`Your Intelligent Symptom Tracker`}
         </Heading>
         <SymptomTracker />
       </ChakraProvider>
